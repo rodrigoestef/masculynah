2054eefd42a2dbc9ce8d9d5ba7406cf4afd10d1c
rodrigoestef <rodrigoestef@gmail.com>
2020-04-22 03:29:31 -0300

    observer body change

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 
-
-document.body.onload = ()=>{
+function masculynah() {
     document.querySelectorAll('img').forEach(e=>{
         e.src = "https://cdn.dicionariopopular.com/imagens/image-193.jpg"
     })
 }
+
+document.body.onload = masculynah()
+
+const observer = new MutationObserver(masculynah)
+
+observer.observe(document.querySelector('body'),{ childList: true,subtree:true})
