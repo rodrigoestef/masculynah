c3fe287f804f98d5f069e51c6d6da848aefdde36
rodrigoestef <rodrigoestef@gmail.com>
2020-04-22 02:51:41 -0300

    script content

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 
 
-console.log("hello");
+document.body.onload = ()=>{
+    document.querySelectorAll('img').forEach(e=>{
+        e.src = "https://cdn.dicionariopopular.com/imagens/image-193.jpg"
+    })
+}
